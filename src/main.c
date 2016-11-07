7fdcb769df22b36d41b2f4d1869d7d564b2e7a8f
David Jackson <david@dwjackson.ca>
2016-11-06 19:37:54 -0500

    Iterate through IFD entries in main

--- src/main.c
+++ src/main.c
@@ -4,17 +4,14 @@
 
 #define USAGE_FMT "Usage: %s [photo_file]\n"
 
-/*
-void
-print_ifd(FILE *fp, long offset, int tiff_offset);
-*/
-
 int
 main(int argc, char *argv[])
 {
     char *file_name;
     exifget_data_t *data;
     int err;
+    struct ifd_entry entry;
+    int retval;
 
     if (argc < 2) {
         printf(USAGE_FMT, argv[0]);
@@ -27,31 +24,15 @@ main(int argc, char *argv[])
         exifget_perror(err);
         exit(EXIT_FAILURE);
     }
-    /* TODO: print IFDs */
+    while ((retval = exifget_next_ifd_entry(data, &entry)) == 0) {
+        /* TODO: print IFDs */
+    }
+    if (retval > 0) {
+        err = retval;
+        exifget_perror(err);
+        exit(EXIT_FAILURE);
+    }
     exifget_close(data);
     
     return 0;
 }
-
-/*
-void
-print_ifd(FILE *fp, long offset, int tiff_offset)
-{
-    int num_ifd_entries;
-    struct ifd_entry *ifd_entries;
-    struct ifd_entry entry;
-    int i;
-
-    fseek(fp, offset + tiff_offset, SEEK_SET);
-    num_ifd_entries = tiff_read_ifd_entries(fp, &ifd_entries, tiff_offset);
-    for (i = 0; i < num_ifd_entries; i++) {
-        entry = ifd_entries[i];
-        if (entry.tag != EXIF_TAG_EXIF_IFD) {
-            print_ifd_entry(&entry, fp, tiff_offset);
-        } else {
-            print_ifd(fp, (long)(entry.data_offset), tiff_offset);
-        }
-    }
-    free(ifd_entries);
-}
-*/
